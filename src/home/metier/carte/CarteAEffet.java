2f141d6390a3854ca001a4d824f1c0c4380f1000
brice <briceorl54580@gmail.com>
2022-02-20 16:58:19 +0100

    🟡 + CarteAEffet & CarteBasique (formatage, commentaire)

--- src/home/metier/carte/CarteAEffet.java
+++ src/home/metier/carte/CarteAEffet.java
@@ -4,10 +4,8 @@ import home.enumeration.ECarteCouleur;
 import home.enumeration.ECarteValeur;
 
 public class CarteAEffet extends Carte {
+
     public CarteAEffet(ECarteCouleur carteCouleur, ECarteValeur carteValeur) {
         super(carteCouleur, carteValeur);
     }
-    /*
-    Vide mais à pour but de mieux comprendre la nature de la carte
-     */
 }
--- src/home/metier/carte/CarteBasique.java
+++ src/home/metier/carte/CarteBasique.java
@@ -4,9 +4,11 @@ import home.enumeration.ECarteCouleur;
 import home.enumeration.ECarteValeur;
 
 public class CarteBasique extends Carte {
+
     public CarteBasique(ECarteCouleur carteCouleur, ECarteValeur carteValeur) {
         super(carteCouleur, carteValeur);
     }
+
     /*
     Vide mais à pour but de mieux comprendre la nature de la carte
      */
