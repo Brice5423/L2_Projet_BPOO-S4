dacb5d99baa6b30655865280fa5fe7136fbefab1
BriceBriçou <briceorl54580@gmail.com>
2022-05-06 22:44:45 +0200

    🟨 -~ Main (code Alicia déplacer & appel 1er test)

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/* ************************************** *
+ *  Projet BPOO | Par Brice.O & Alicia.O  *
+ *  Version Java : 17.0.2                 *
+ * ************************************** */
+
+import tests.CoupsLegauxCartesSimples;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        /* ***** Tests coups légaux avec des cartes simples ***** */
+        CoupsLegauxCartesSimples.executionDuTest();
+
+        // ...
+    }
+}
