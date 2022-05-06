8bc49e9590875bc416518a677dba493aeead02ea
BriceBriçou <briceorl54580@gmail.com>
2022-05-07 00:44:22 +0200

    🟨 ~ CoupsLegauxCartesSimples (format, correction & validée)

--- src/tests/CoupsLegauxCartesSimples.java
+++ src/tests/CoupsLegauxCartesSimples.java
@@ -2,6 +2,7 @@ package tests;
 
 import home.enumeration.ECarteCouleur;
 import home.enumeration.ECarteValeur;
+import home.excepcion.ExpertManquantException;
 import home.expert.*;
 import home.metier.Joueur;
 import home.metier.Partie;
@@ -13,7 +14,7 @@ import java.util.ArrayList;
 public class CoupsLegauxCartesSimples {
     /**
      * Alice joue une carte de la bonne couleur.
-     *
+     * <p>
      * 1) Vérifier que le joueur courant est bien Alice,
      * 2) Vérifier qu'Alice possède bien 3 cartes,
      * 3) Alice joue le « 2 Vert »,
@@ -25,21 +26,17 @@ public class CoupsLegauxCartesSimples {
      * 9) Vérifier que le joueur courant est Bob.
      * _____________________________________________________________________________
      * Bob joue une carte de couleur différente, mais de même valeur.
-     *
-     *  1) Vérifier	que	Bob	possède	bien 3 cartes,
-     *  2) Bob pose le « 2 bleu »,
-     *  3) Vérifier que	Bob	possède	bien 2 cartes,
-     *  4) Vérifier que	les cartes de Bob sont le « 4 jaune » et le	« 9	rouge »,
-     *  5) Vérifier que la carte au sommet du tas est le « 2 Bleu »,
-     *  6) Vérifier	que le nombre de cartes du tas est 3,
-     *  7) Bob finit le tour,
-     *  8) Vérifier que le joueur courant est Charles.
+     * <p>
+     * 1) Vérifier	que	Bob	possède	bien 3 cartes,
+     * 2) Bob pose le « 2 bleu »,
+     * 3) Vérifier que	Bob	possède	bien 2 cartes,
+     * 4) Vérifier que	les cartes de Bob sont le « 4 jaune » et le	« 9	rouge »,
+     * 5) Vérifier que la carte au sommet du tas est le « 2 Bleu »,
+     * 6) Vérifier	que le nombre de cartes du tas est 3,
+     * 7) Bob finit le tour,
+     * 8) Vérifier que le joueur courant est Charles.
      */
     public static void executionDuTest() {
-        /* ***** ***** Initialisation des experts ***** ***** */
-
-        Expert lesExperts = Expert.initialiseTousLesExperts();
-
         /* ***** ***** Initialiser le fichier test ***** ***** */
 
         ArrayList<Carte> pioche = new ArrayList<Carte>();
@@ -52,50 +49,57 @@ public class CoupsLegauxCartesSimples {
 
         Partie partie = new Partie(pioche);
 
-        Joueur Alice = new Joueur("Alice");
-        Joueur Bob = new Joueur("Bob");
-        Joueur Charles = new Joueur("Charles");
+        Joueur alice = new Joueur("Alice");
+        Joueur bob = new Joueur("Bob");
+        Joueur charles = new Joueur("Charles");
 
-        partie.ajoutJoueurPartie(Alice);
-        partie.ajoutJoueurPartie(Bob);
-        partie.ajoutJoueurPartie(Charles);
+        partie.ajoutJoueurPartie(alice);
+        partie.ajoutJoueurPartie(bob);
+        partie.ajoutJoueurPartie(charles);
 
         ArrayList<Carte> Tas = new ArrayList<Carte>();
 
         Tas.add(new CarteBasique(ECarteCouleur.VERT, ECarteValeur.HUIT));
-
         partie.setDepot(Tas);
+        partie.setCarteReference(partie.carteDepot());
 
-        Alice.recupererCarte(new CarteBasique(ECarteCouleur.VERT, ECarteValeur.DEUX));
-        Alice.recupererCarte(new CarteBasique(ECarteCouleur.JAUNE, ECarteValeur.SIX));
-        Alice.recupererCarte(new CarteBasique(ECarteCouleur.ROUGE, ECarteValeur.UN));
+        Carte vertDeux = new CarteBasique(ECarteCouleur.VERT, ECarteValeur.DEUX);
+        Carte jauneSix = new CarteBasique(ECarteCouleur.JAUNE, ECarteValeur.SIX);
 
-        Bob.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.DEUX));
-        Bob.recupererCarte(new CarteBasique(ECarteCouleur.JAUNE, ECarteValeur.QUATRE));
-        Bob.recupererCarte(new CarteBasique(ECarteCouleur.ROUGE, ECarteValeur.NEUF));
+        alice.recupererCarte(vertDeux);
+        alice.recupererCarte(jauneSix);
+        alice.recupererCarte(new CarteBasique(ECarteCouleur.ROUGE, ECarteValeur.UN));
 
-        Charles.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.NEUF));
-        Charles.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.SEPT));
-        Charles.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.ZERO));
+        bob.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.DEUX));
+        bob.recupererCarte(new CarteBasique(ECarteCouleur.JAUNE, ECarteValeur.QUATRE));
+        bob.recupererCarte(new CarteBasique(ECarteCouleur.ROUGE, ECarteValeur.NEUF));
+
+        charles.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.NEUF));
+        charles.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.SEPT));
+        charles.recupererCarte(new CarteBasique(ECarteCouleur.BLEU, ECarteValeur.ZERO));
 
 
         /* ***** ***** Debut test : Alice joue une carte de la bonne couleur. ***** ***** */
 
         // 1) Vérifier que le joueur courant est bien Alice,
-        if (partie.joueurCourant().equals(Alice)) {
+        if (partie.joueurCourant().equals(alice)) {
             System.out.println("Le joueur courant est bien Alice");
         }
 
         // 2) Vérifier qu'Alice possède bien 3 cartes,
-        if (Alice.nbCarteEnMain() == 3) {
+        if (alice.nbCarteEnMain() == 3) {
             System.out.println("Alice possède bien 3 cartes");
         }
 
         // 3) Alice joue le « 2 Vert »,
-        partie.deposerCarteDepot(Alice.poserCarte(new CarteBasique(ECarteCouleur.VERT,ECarteValeur.DEUX)));
+        try {
+            partie.deposerCarteDepot(alice.poserCarte(vertDeux));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // 4) Vérifier qu'Alice possède bien 2 cartes,
-        if (Alice.nbCarteEnMain() == 2) {
+        if (alice.nbCarteEnMain() == 2) {
             System.out.println("Alice possède bien 2 cartes");
         }
 
