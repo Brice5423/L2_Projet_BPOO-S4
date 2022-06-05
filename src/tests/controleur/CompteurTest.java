e02a8539634a938e286e2ba71eb16e0e9828423b
Brice Briçou <46904213+Brice5423@users.noreply.github.com>
2022-06-05 22:51:33 +0200

    🟧 +~ CompteurTest (JavaDoc)

--- src/tests/controleur/CompteurTest.java
+++ src/tests/controleur/CompteurTest.java
@@ -1,25 +1,45 @@
 package tests.controleur;
 
+/**
+ * Class pour le controller de réussite du programme.
+ */
 public class CompteurTest {
+    /** Le nom du test */
     private final String nomTest;
+    /** Le nombre de tests total */
     private int nbTestTotal;
+    /** Le nombre de tests réussi */
     private int nbTestOk;
 
+    /**
+     * Constructeur pour faire un suivi d'un test.
+     * @param nomTest nom du test
+     */
     public CompteurTest(String nomTest) {
         this.nomTest = nomTest;
         this.nbTestTotal = 0;
         this.nbTestOk = 0;
     }
 
+    /**
+     * Test valide.
+     */
     public void testOK() {
         this.nbTestTotal++;
         this.nbTestOk++;
     }
 
+    /**
+     * Test faux.
+     */
     public void testFaux() {
         this.nbTestTotal++;
     }
 
+    /**
+     * Affiche le nombre de tests total, le nombre de tests OK avec le taux de réussite des testes.
+     * @return true : 100% OK / false : < 100% OK
+     */
     public boolean afficheResultatsTest() {
         System.out.println("\n\tRésultats du test : " + this.nomTest);
         System.out.println("Nb test total : " + this.nbTestTotal);
