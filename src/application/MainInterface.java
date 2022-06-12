f6f89f87b80ba9d65aef7960db605d7df496fcd1
Brice Briçou <46904213+Brice5423@users.noreply.github.com>
2022-06-12 14:15:43 +0200

    🟩 ~- MainInterface (commentaire, format)

--- src/application/MainInterface.java
+++ src/application/MainInterface.java
@@ -27,8 +27,8 @@ public class MainInterface extends Application {
     private static final int L_CANVAS = 400;
     private static final int L_CARTE = 80;
     private static final int ECART = 30;
-    private Canvas canSabot;
 
+    private Canvas canSabot;
     private Partie partie;
 
     public static void main(String[] args) {
@@ -140,7 +140,7 @@ public class MainInterface extends Application {
             this.dessinerSabot();
 
             if (mainDuJoueur.isEmpty()) {
-                /* ***** Boite de dialogue, pour mettre fin au jeu ***** */ // TODO Faire en sorte que le jeu puisse ce finir.
+                /* ***** Boite de dialogue, pour mettre fin au jeu ***** */
                 // Créer la boite avec le dialogue
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
                 alert.setTitle("Victoire de " + joueur.getNom() + " !");
@@ -161,10 +161,6 @@ public class MainInterface extends Application {
     }
 
     private void dessinerMain(Canvas canvas, ArrayList<Carte> mainDuJoueur) {
-        /* Liste est une liste de chaines de caractère.
-         * Mais vous devriez sans doute utiliser vos propres classes, pas des Strings !
-         */
-
         canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
         int nbCartes = mainDuJoueur.size();
@@ -178,11 +174,6 @@ public class MainInterface extends Application {
     }
 
     private HBox initBoutonUno(Canvas canMain, Joueur joueur) {
-        /* TODO Voir si on a bien panser à tout
-         * Cette partie est sans doute incomplète. Il y a sans doute d'autres actions à
-         * prévoir que piocher et dire uno !
-         */
-
         HBox hBox = new HBox();
         hBox.setAlignment(Pos.CENTER);
 
